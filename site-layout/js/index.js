ec2f5c2e222aa9cef6c19de9b56d55a2e4c0c43e
ManTT-12 <lanhbang312@gmail.com>
2022-06-15 17:44:52 +0700

    update

--- site-layout/js/index.js
+++ site-layout/js/index.js
@@ -1,8 +1,10 @@
 function toggleMenu() {
-    let toggle = document.querySelector('#nav-mobi-btn');
-    let navigation = document.querySelector('#nav-mobi');
-    let close = document.querySelector('#nav-mobi-close');
+    let toggle = document.querySelector('.nav_toggle');
+    let navigation = document.querySelector('.nav_mobi');
+    let close = document.querySelector('.nav_close');
+    let brand = document.querySelector('.nav_brand');
     toggle.classList.toggle('active');
     navigation.classList.toggle('active');
+    brand.classList.toggle('active');
     close.classList.toggle('active');
 }
